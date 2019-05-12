1e1f6389cd36ab8ba54fe38274b78a8246c53bc2
Xavier Lepretre <xavier.lepretre@gmail.com>
2019-05-12 23:09:39 +0100

    Reworked BlindBetFlow to collect states from players.

--- src/main/kotlin/org/cordacodeclub/bluff/contract/CardStateContract.kt
+++ src/main/kotlin/org/cordacodeclub/bluff/contract/CardStateContract.kt
@@ -1,7 +1,14 @@
 package org.cordacodeclub.bluff.contract
 
 import net.corda.core.contracts.Contract
+import net.corda.core.transactions.LedgerTransaction
 
 class CardStateContract : Contract {
+    companion object {
+        val ID = "org.cordacodeclub.bluff.contract.CardStateContract"
+    }
 
+    override fun verify(tx: LedgerTransaction) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
 }
--- src/main/kotlin/org/cordacodeclub/bluff/flow/BlindBetFlow.kt
+++ src/main/kotlin/org/cordacodeclub/bluff/flow/BlindBetFlow.kt
@@ -1,36 +1,58 @@
 package org.cordacodeclub.bluff.flow
 
 import co.paralleluniverse.fibers.Suspendable
+import net.corda.core.contracts.Command
+import net.corda.core.contracts.StateAndRef
+import net.corda.core.contracts.requireThat
 import net.corda.core.flows.*
 import net.corda.core.identity.Party
+import net.corda.core.node.services.Vault
+import net.corda.core.node.services.queryBy
+import net.corda.core.node.services.vault.QueryCriteria
+import net.corda.core.node.services.vault.builder
+import net.corda.core.serialization.CordaSerializable
+import net.corda.core.transactions.FilteredTransaction
 import net.corda.core.transactions.SignedTransaction
+import net.corda.core.transactions.TransactionBuilder
 import net.corda.core.utilities.ProgressTracker
-import org.cordacodeclub.bluff.state.BlindBetState
-import org.cordacodeclub.bluff.state.BlindBetStatus
+import net.corda.core.utilities.unwrap
+import org.cordacodeclub.bluff.contract.TokenContract
+import org.cordacodeclub.bluff.state.PotState
+import org.cordacodeclub.bluff.state.TokenSchemaV1
 import org.cordacodeclub.bluff.state.TokenState
 
 //Initial flow
 object BlindBetFlow {
 
+    @CordaSerializable
+    class BlindBetRequest(val minter: Party, val amount: Long);
+
+    @CordaSerializable
+    class Combo(val request: BlindBetRequest, val filteredTransaction: FilteredTransaction);
+
     @InitiatingFlow
     @StartableByRPC
-
     /**
      * This flow is startable by the dealer party.
      * And it has to be signed by the players and delear.
      * @param players list of parties starting the game
      */
+    class Initiator(val players: List<Party>, val minter: Party, val smallBet: Long) : FlowLogic<SignedTransaction>() {
 
-    class Initiator(val players: List<Party>) {
+        init {
+            requireThat {
+                "There needs at least 2 players" using (players.size >= 2)
+            }
+        }
 
         /**
          * The progress tracker checkpoints each stage of the flow and outputs the specified messages when each
          * checkpoint is reached in the code. See the 'progressTracker.currentStep' expressions within the call() function.
          */
         companion object {
-            object GENERATING_BLINDBET_STATES : ProgressTracker.Step("Generating blind based on the players.")
-            object GENERATING_TOKEN_STATES : ProgressTracker.Step("Generating tokens based on the players.")
-            object GENERATING_CARD_STATES : ProgressTracker.Step("Generating cards based on the players.")
+            object COLLECTING_SMALL_BLINDBET_STATES : ProgressTracker.Step("Collecting small blind bets from player 1.")
+            object COLLECTING_BIG_BLINDBET_STATES : ProgressTracker.Step("Collecting big blind bets from player 2.")
+//            object GENERATING_CARD_STATES : ProgressTracker.Step("Generating cards based on the players.")
             object GENERATING_POT_STATES : ProgressTracker.Step("Generating betting pot.")
             object GENERATING_TRANSACTION : ProgressTracker.Step("Generating transaction based on new IOU.")
             object VERIFYING_TRANSACTION : ProgressTracker.Step("Verifying contract constraints.")
@@ -40,40 +62,186 @@ object BlindBetFlow {
                 override fun childProgressTracker() = CollectSignaturesFlow.tracker()
             }
 
-            object FINALISING_TRANSACTION : ProgressTracker.Step("Obtaining notary signature and recording transaction.") {
+            object FINALISING_TRANSACTION :
+                ProgressTracker.Step("Obtaining notary signature and recording transaction.") {
                 override fun childProgressTracker() = FinalityFlow.tracker()
             }
         }
 
-            fun tracker() = ProgressTracker(
-                    GENERATING_BLINDBET_STATES,
-                    GENERATING_TOKEN_STATES,
-                    GENERATING_CARD_STATES,
-                    GENERATING_POT_STATES,
-                    GENERATING_TRANSACTION,
-                    VERIFYING_TRANSACTION,
-                    SIGNING_TRANSACTION,
-                    GATHERING_SIGS,
-                    FINALISING_TRANSACTION)
+        fun tracker() = ProgressTracker(
+            COLLECTING_SMALL_BLINDBET_STATES,
+            COLLECTING_BIG_BLINDBET_STATES,
+//            GENERATING_CARD_STATES,
+            GENERATING_POT_STATES,
+            GENERATING_TRANSACTION,
+            VERIFYING_TRANSACTION,
+            SIGNING_TRANSACTION,
+            GATHERING_SIGS,
+            FINALISING_TRANSACTION
+        )
 
-            override val progressTracker = tracker()
-
-
-            /**
-             * The flow logic is encapsulated within the call() method.
-             */
-            @Suspendable
-            override fun call(): SignedTransaction {
-
-                // Obtain a reference to the notary we want to use.
-                val notary = serviceHub.networkMapCache.notaryIdentities[0]
-
-                progressTracker.currentStep = GENERATING_BLINDBET_STATES
+        override val progressTracker = tracker()
 
 
-                val tokenStates =  players.map { BlindBetState(sb = 10, owner = it, status = BlindBetStatus.PASS) }
-                val tokenState =  players.map { TokenState() }
+        /**
+         * The flow logic is encapsulated within the call() method.
+         */
+        @Suspendable
+        override fun call(): SignedTransaction {
+
+            // Obtain a reference to the notary we want to use.
+            val notary = serviceHub.networkMapCache.notaryIdentities.first()
+
+            progressTracker.currentStep = COLLECTING_SMALL_BLINDBET_STATES
+
+            val requiredSigners = players.take(2)
+            val first2Flows = requiredSigners.map { initiateFlow(it) }
+            val request = BlindBetRequest(minter = minter, amount = smallBet)
+            val states0 = first2Flows.get(0).sendAndReceive<List<StateAndRef<TokenState>>>(request).unwrap { it }
+                .filter { it.state.data.minter == minter }
+            val sum0 = states0.fold(0L) { sum, state ->
+                sum + state.state.data.amount
+            }
+            requireThat {
+                "We have to receive at least $smallBet" using (smallBet <= sum0)
             }
 
+            progressTracker.currentStep = COLLECTING_BIG_BLINDBET_STATES
+            val states1 = first2Flows.get(1).sendAndReceive<List<StateAndRef<TokenState>>>(request).unwrap { it }
+                .filter { it.state.data.minter == minter }
+            val sum1 = states1.fold(0L) { sum, state ->
+                sum + state.state.data.amount
+            }
+            requireThat {
+                "We have to receive at least $sum0" using (sum0 <= sum1)
+                // TODO enforce the doubling strictly?
+            }
+
+            val command = Command(
+                TokenContract.Commands.BetToPot(),
+                requiredSigners.map { it.owningKey }
+            )
+
+            progressTracker.currentStep = GENERATING_POT_STATES
+            val potState0 = PotState(minter = minter, amount = sum0)
+            // We separate them to accomodate future owner tracking
+            val potState1 = PotState(minter = minter, amount = sum1)
+
+            progressTracker.currentStep = GENERATING_TRANSACTION
+            val txBuilder = TransactionBuilder(notary = notary)
+
+            states0.forEach { txBuilder.addInputState(it) }
+            states1.forEach { txBuilder.addInputState(it) }
+            txBuilder.addOutputState(potState0, TokenContract.ID)
+            txBuilder.addOutputState(potState1, TokenContract.ID)
+
+            progressTracker.currentStep = VERIFYING_TRANSACTION
+            txBuilder.verify(serviceHub)
+
+            progressTracker.currentStep = SIGNING_TRANSACTION
+            val signedTx = serviceHub.signInitialTransaction(txBuilder)
+
+            progressTracker.currentStep = GATHERING_SIGS
+            val fullySignedTx = subFlow(
+                CollectSignaturesFlow(
+                    signedTx,
+                    first2Flows,
+                    GATHERING_SIGS.childProgressTracker()
+                )
+            )
+
+            progressTracker.currentStep = FINALISING_TRANSACTION
+            return subFlow(
+                FinalityFlow(
+                    fullySignedTx,
+                    FINALISING_TRANSACTION.childProgressTracker()
+                )
+            )
         }
     }
+
+    @InitiatedBy(Initiator::class)
+    class CollectorAndSigner(val otherPartySession: FlowSession) : FlowLogic<SignedTransaction>() {
+
+        /**
+         * The progress tracker checkpoints each stage of the flow and outputs the specified messages when each
+         * checkpoint is reached in the code. See the 'progressTracker.currentStep' expressions within the call() function.
+         */
+        companion object {
+            object RECEIVING_REQUEST_FOR_STATES : ProgressTracker.Step("Receiving request for token states.")
+            object SENDING_TOKEN_STATES : ProgressTracker.Step("Sending back token states.")
+            object SIGNING_TRANSACTION : ProgressTracker.Step("Signing transaction with our private key.") {
+                override fun childProgressTracker(): ProgressTracker {
+                    return SignTransactionFlow.tracker()
+                }
+            }
+
+            object CHECKING_VALIDITY : ProgressTracker.Step("Checking transaction validity.")
+
+            fun tracker() = ProgressTracker(
+                RECEIVING_REQUEST_FOR_STATES,
+                SENDING_TOKEN_STATES,
+                SIGNING_TRANSACTION,
+                CHECKING_VALIDITY
+            )
+        }
+
+        override val progressTracker: ProgressTracker = tracker()
+
+        @Suspendable
+        override fun call(): SignedTransaction {
+
+            progressTracker.currentStep = RECEIVING_REQUEST_FOR_STATES
+            // Receive an incomplete transaction and a minimum amount to bet
+            val received = otherPartySession.receive<BlindBetRequest>().unwrap { it }
+
+            // TODO What to check with the filtered transaction?
+
+            // Find the states to bet according to the amount requested
+            var remainingAmount = received.amount
+            val unconsumedStates = builder {
+                val forMinter = TokenSchemaV1.PersistentToken::minter.equal(received.minter.toString())
+                val forOwner =
+                    TokenSchemaV1.PersistentToken::owner.equal(serviceHub.myInfo.legalIdentities.first().toString())
+                val minterCriteria = QueryCriteria.VaultCustomQueryCriteria(forMinter)
+                val ownerCriteria = QueryCriteria.VaultCustomQueryCriteria(forOwner)
+                val unconsumedCriteria: QueryCriteria =
+                    QueryCriteria.VaultQueryCriteria(status = Vault.StateStatus.UNCONSUMED)
+                val criteria = unconsumedCriteria.and(minterCriteria).and(ownerCriteria)
+                serviceHub.vaultService.queryBy<TokenState>(criteria).states
+            }.takeWhile {
+                // TODO avoid paying more than necessary
+                remainingAmount -= it.state.data.amount
+                remainingAmount > 0
+            }
+
+            // TODO soft lock the unconsumed states?
+
+            progressTracker.currentStep = SENDING_TOKEN_STATES
+            otherPartySession.send(unconsumedStates)
+
+            progressTracker.currentStep = SIGNING_TRANSACTION
+            val signTransactionFlow =
+                object : SignTransactionFlow(otherPartySession, SIGNING_TRANSACTION.childProgressTracker()) {
+
+                    override fun checkTransaction(stx: SignedTransaction) = requireThat {
+                        progressTracker.currentStep = CHECKING_VALIDITY
+                        // Making sure we see our previously picked states
+                        val receivedInputRefs = stx.coreTransaction.inputs.map {
+                            serviceHub.toStateAndRef<TokenState>(it)
+                        }.filter {
+                            it.state.data.owner == serviceHub.myInfo.legalIdentities.first()
+                        }.map {
+                            it.ref
+                        }
+
+                        val previouslySent = unconsumedStates.map { it.ref }
+                        "We should receive the same inputs" using (
+                                receivedInputRefs.containsAll(previouslySent) &&
+                                        previouslySent.containsAll(receivedInputRefs))
+                    }
+                }
+            return subFlow(signTransactionFlow)
+        }
+    }
+}
