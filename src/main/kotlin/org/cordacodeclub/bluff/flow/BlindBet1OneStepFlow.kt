dc6bf805bd6e17b5ece98ac049fa415ff525f6cc
Xavier Lepretre <xavier.lepretre@gmail.com>
2019-06-23 22:10:43 +0100

    Added a flow for first blind bet.

---
+++ src/main/kotlin/org/cordacodeclub/bluff/flow/BlindBet1OneStepFlow.kt
@@ -0,0 +1,239 @@
+package org.cordacodeclub.bluff.flow
+
+import co.paralleluniverse.fibers.Suspendable
+import net.corda.core.contracts.Command
+import net.corda.core.contracts.requireThat
+import net.corda.core.crypto.SecureHash
+import net.corda.core.flows.*
+import net.corda.core.identity.Party
+import net.corda.core.serialization.CordaSerializable
+import net.corda.core.transactions.SignedTransaction
+import net.corda.core.transactions.TransactionBuilder
+import net.corda.core.utilities.ProgressTracker
+import net.corda.core.utilities.unwrap
+import org.cordacodeclub.bluff.contract.OneStepContract
+import org.cordacodeclub.bluff.contract.TokenContract
+import org.cordacodeclub.bluff.dealer.CardDeckDatabaseService
+import org.cordacodeclub.bluff.dealer.CardDeckInfo
+import org.cordacodeclub.bluff.dealer.IncompleteCardDeckInfo
+import org.cordacodeclub.bluff.player.PlayerAction
+import org.cordacodeclub.bluff.round.BettingRound
+import org.cordacodeclub.bluff.state.PlayedAction
+import org.cordacodeclub.bluff.state.RoundState
+import org.cordacodeclub.bluff.state.TokenState
+
+object BlindBet1OneStepFlow {
+
+    @CordaSerializable
+    data class CardDeckCreateRequest(val dealer: Party, val players: List<Party>)
+
+    @InitiatingFlow
+    @StartableByRPC
+    /**
+     * This flow is started by the player doing the first blind bet. Tx has to be signed by the dealer.
+     * @param players list of parties starting the game
+     * @param minter the desired minter of the tokens
+     * @param dealer the desired dealer of the deck of cards
+     * @param smallBet the starting small bet
+     * @param progressTracker the overridden progress tracker
+     */
+    class Initiator(
+        val players: List<Party>,
+        val minter: Party,
+        val dealer: Party,
+        val smallBet: Long,
+        override val progressTracker: ProgressTracker = tracker()
+    ) :
+        FlowLogic<SignedTransaction>() {
+
+        init {
+            require(smallBet > 0) { "SmallBet should not be 0" }
+            require(players.size >= RoundState.MIN_PLAYER_COUNT) {
+                "There should be at least ${RoundState.MIN_PLAYER_COUNT} players"
+            }
+            require(!players.contains(dealer)) { "The dealer cannot play" }
+        }
+
+        companion object {
+            object CREATING_NEW_DECK : ProgressTracker.Step("Asking dealer to create a new deck.")
+            object COLLECTING_TOKENS : ProgressTracker.Step("Collecting own tokens for blind bet.") {
+                override fun childProgressTracker(): ProgressTracker {
+                    return CollectOwnTokenStateFlow.tracker()
+                }
+            }
+
+            object GENERATING_POT_STATES : ProgressTracker.Step("Generating betting pot.")
+            object GENERATING_TRANSACTION : ProgressTracker.Step("Generating transaction.")
+            object VERIFYING_TRANSACTION : ProgressTracker.Step("Verifying contract constraints.")
+            object SIGNING_TRANSACTION : ProgressTracker.Step("Signing transaction with our private key.")
+            object GATHERING_SIGS : ProgressTracker.Step("Gathering the counterparty's signature.") {
+                override fun childProgressTracker() = CollectSignaturesFlow.tracker()
+            }
+
+            object FINALISING_TRANSACTION :
+                ProgressTracker.Step("Obtaining notary signature and recording transaction.") {
+                override fun childProgressTracker() = FinalityFlow.tracker()
+            }
+
+            fun tracker() = ProgressTracker(
+                CREATING_NEW_DECK,
+                COLLECTING_TOKENS,
+                GENERATING_POT_STATES,
+                GENERATING_TRANSACTION,
+                VERIFYING_TRANSACTION,
+                SIGNING_TRANSACTION,
+                GATHERING_SIGS,
+                FINALISING_TRANSACTION
+            )
+        }
+
+        @Suspendable
+        override fun call(): SignedTransaction {
+
+            val me = serviceHub.myInfo.legalIdentities.first()
+
+            progressTracker.currentStep = CREATING_NEW_DECK
+            val createRequest = CardDeckCreateRequest(dealer, players)
+            val playerFlows = players.minus(me).toSet().map { initiateFlow(it) }
+            val dealerFlow = initiateFlow(dealer)
+            // Priming the players too, even though only the dealer returns anything
+            playerFlows.plus(dealerFlow).forEach { it.send(createRequest) }
+            val deckRootHash = dealerFlow.receive<SecureHash>().unwrap { it }
+
+            progressTracker.currentStep = COLLECTING_TOKENS
+            val tokenStates = subFlow(
+                CollectOwnTokenStateFlow(
+                    TokenState(minter, me, smallBet, false),
+                    COLLECTING_TOKENS.childProgressTracker()
+                )
+            )
+
+            val notary = tokenStates.map { it.state.notary }.toSet().singleOrNull()
+                ?: throw FlowException("Did not collect tokens from a single notary")
+
+            progressTracker.currentStep = GENERATING_POT_STATES
+            // We separate them to accommodate future owner tracking
+            val potState = tokenStates.map { it.state.data.amount }.sum()
+                .let { sum ->
+                    tokenStates.first().state.data.copy(amount = sum, isPot = true)
+                }
+
+            progressTracker.currentStep = GENERATING_TRANSACTION
+            val txBuilder = TransactionBuilder(notary = notary)
+
+            txBuilder.addCommand(Command(TokenContract.Commands.BetToPot(), me.owningKey))
+            tokenStates.forEach { txBuilder.addInputState(it) }
+            txBuilder.addOutputState(potState, TokenContract.ID)
+
+            txBuilder.addCommand(Command(OneStepContract.Commands.BetBlind1(), listOf(dealer.owningKey)))
+            txBuilder.addOutputState(
+                RoundState(
+                    minter = minter,
+                    dealer = dealer,
+                    deckRootHash = deckRootHash,
+                    roundType = BettingRound.BLIND_BET_1,
+                    currentPlayerIndex = players.indexOf(me),
+                    players = players.map {
+                        PlayedAction(it, if (it == me) PlayerAction.Raise else PlayerAction.Missing)
+                    },
+                    lastRaiseIndex = players.indexOf(me)
+                ),
+                OneStepContract.ID
+            )
+
+            progressTracker.currentStep = VERIFYING_TRANSACTION
+            txBuilder.verify(serviceHub)
+
+            progressTracker.currentStep = SIGNING_TRANSACTION
+            val signedTx = serviceHub.signInitialTransaction(txBuilder)
+
+            progressTracker.currentStep = GATHERING_SIGS
+            val fullySignedTx = subFlow(
+                CollectSignaturesFlow(signedTx, listOf(dealerFlow), GATHERING_SIGS.childProgressTracker())
+            )
+
+            progressTracker.currentStep = FINALISING_TRANSACTION
+            return subFlow(
+                FinalityFlow(
+                    fullySignedTx,
+                    playerFlows.plus(dealerFlow),
+                    FINALISING_TRANSACTION.childProgressTracker()
+                )
+            )
+        }
+    }
+
+    @InitiatedBy(Initiator::class)
+    class Responder(val otherPartySession: FlowSession) : FlowLogic<SignedTransaction>() {
+
+        companion object {
+            object RECEIVING_REQUEST_FOR_DECK : ProgressTracker.Step("Receiving request for deck of cards.")
+            object CREATING_CARD_DECK : ProgressTracker.Step("Creating deck of cards.")
+            object SIGNING_TRANSACTION : ProgressTracker.Step("Signing transaction with our private key.") {
+                override fun childProgressTracker(): ProgressTracker {
+                    return SignTransactionFlow.tracker()
+                }
+            }
+
+            object RECEIVING_FINALISED_TRANSACTION : ProgressTracker.Step("Receiving finalised transaction.")
+
+            fun tracker() = ProgressTracker(
+                RECEIVING_REQUEST_FOR_DECK,
+                CREATING_CARD_DECK,
+                SIGNING_TRANSACTION,
+                RECEIVING_FINALISED_TRANSACTION
+            )
+        }
+
+        override val progressTracker: ProgressTracker = tracker()
+
+        @Suspendable
+        override fun call(): SignedTransaction {
+
+            val me = serviceHub.myInfo.legalIdentities.first()
+
+            progressTracker.currentStep = RECEIVING_REQUEST_FOR_DECK
+
+            val request = otherPartySession.receive<CardDeckCreateRequest>().unwrap { it }
+            val txId =
+                if (request.dealer == me) dealerResponse(request).id
+                else playerResponse()
+
+            progressTracker.currentStep = RECEIVING_FINALISED_TRANSACTION
+            return subFlow(ReceiveFinalityFlow(otherPartySession, expectedTxId = txId))
+        }
+
+        @Suspendable
+        fun dealerResponse(request: CardDeckCreateRequest): SignedTransaction {
+            progressTracker.currentStep = CREATING_CARD_DECK
+            val deck = CardDeckInfo.createShuffledWith(request.players.map { it.name }, request.dealer.name)
+            serviceHub.cordaService(CardDeckDatabaseService::class.java).safeAddIncompleteDeck(
+                IncompleteCardDeckInfo(deck)
+            )
+            otherPartySession.send(deck.rootHash)
+
+            progressTracker.currentStep = SIGNING_TRANSACTION
+            val signTransactionFlow =
+                object : SignTransactionFlow(otherPartySession, SIGNING_TRANSACTION.childProgressTracker()) {
+
+                    override fun checkTransaction(stx: SignedTransaction) = requireThat {
+                        val state = stx.coreTransaction.outputsOfType<RoundState>().firstOrNull()
+                            ?: throw FlowException("There is more than one RoundState")
+                        "We should be the dealer" using (state.dealer == request.dealer)
+                        "The players should be the same as in the request" using
+                                (request.players.toSet() == state.players.map { it.player }.toSet())
+                        "We should have our created deck" using (state.deckRootHash == deck.rootHash)
+                    }
+                }
+            return subFlow(signTransactionFlow)
+        }
+
+        @Suspendable
+        fun playerResponse(): SecureHash? {
+            // Players do not respond
+            progressTracker.currentStep = CREATING_CARD_DECK
+            progressTracker.currentStep = SIGNING_TRANSACTION
+            return null
+        }
+    }
+}
---
+++ src/test/kotlin/org/cordacodeclub/bluff/flow/BlindBet1OneStepFlowTest.kt
@@ -0,0 +1,187 @@
+package org.cordacodeclub.bluff.flow
+
+import net.corda.core.contracts.ContractState
+import net.corda.core.identity.CordaX500Name
+import net.corda.core.identity.Party
+import net.corda.core.transactions.SignedTransaction
+import net.corda.core.utilities.getOrThrow
+import net.corda.testing.core.singleIdentity
+import net.corda.testing.node.MockNetwork
+import net.corda.testing.node.StartedMockNode
+import org.cordacodeclub.bluff.dealer.CardDeckDatabaseService
+import org.cordacodeclub.bluff.player.PlayerAction
+import org.cordacodeclub.bluff.round.BettingRound
+import org.cordacodeclub.bluff.state.PlayedAction
+import org.cordacodeclub.bluff.state.RoundState
+import org.cordacodeclub.bluff.state.TokenState
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
+
+/**
+ * Add -javaagent:./lib/quasar.jar to VM Options
+ */
+class BlindBet1OneStepFlowTest {
+    private lateinit var network: MockNetwork
+    private lateinit var minterNode: StartedMockNode
+    private lateinit var dealerNode: StartedMockNode
+    private lateinit var player0Node: StartedMockNode
+    private lateinit var player1Node: StartedMockNode
+    private lateinit var player2Node: StartedMockNode
+    private lateinit var player3Node: StartedMockNode
+    private lateinit var minter: Party
+    private lateinit var dealer: Party
+    private lateinit var player0: Party
+    private lateinit var player1: Party
+    private lateinit var player2: Party
+    private lateinit var player3: Party
+    private lateinit var mintTx: SignedTransaction
+
+    @Before
+    fun setup() {
+        network = MockNetwork(
+            listOf(
+                "org.cordacodeclub.bluff.contract",
+                "org.cordacodeclub.bluff.dealer",
+                "org.cordacodeclub.bluff.flow",
+                "org.cordacodeclub.bluff.state"
+            )
+        )
+        minterNode = network.createPartyNode(CordaX500Name.parse("O=Minter, L=London, C=GB"))
+        dealerNode = network.createPartyNode(CordaX500Name.parse("O=Dealer, L=London, C=GB"))
+        player0Node = network.createPartyNode(CordaX500Name.parse("O=Player0, L=London, C=GB"))
+        player1Node = network.createPartyNode(CordaX500Name.parse("O=Player1, L=London, C=GB"))
+        player2Node = network.createPartyNode(CordaX500Name.parse("O=Player2, L=London, C=GB"))
+        player3Node = network.createPartyNode(CordaX500Name.parse("O=Player3, L=London, C=GB"))
+        minter = minterNode.info.singleIdentity()
+        dealer = dealerNode.info.singleIdentity()
+        player0 = player0Node.info.singleIdentity()
+        player1 = player1Node.info.singleIdentity()
+        player2 = player2Node.info.singleIdentity()
+        player3 = player3Node.info.singleIdentity()
+
+        val mintFlow = MintTokenFlow.Minter(listOf(player0, player1, player2, player3), 100, 1)
+        val future = minterNode.startFlow(mintFlow)
+        network.runNetwork()
+        mintTx = future.getOrThrow()
+    }
+
+    @After
+    fun tearDown() {
+        network.stopNodes()
+    }
+
+    @Test
+    fun `SignedTransaction is signed by blind bet player and dealer`() {
+        val flow = BlindBet1OneStepFlow.Initiator(
+            players = listOf(player0, player1, player2, player3),
+            minter = minter, dealer = dealer, smallBet = 4
+        )
+        val future = player1Node.startFlow(flow)
+        network.runNetwork()
+
+        val signedTx = future.getOrThrow()
+        signedTx.sigs.map { it.by }.toSet().also {
+            assertTrue(it.contains(dealer.owningKey))
+            assertFalse(it.contains(player0.owningKey))
+            assertTrue(it.contains(player1.owningKey))
+            assertFalse(it.contains(player2.owningKey))
+            assertFalse(it.contains(player3.owningKey))
+        }
+    }
+
+    @Test
+    fun `SignedTransaction has inputs from first player only`() {
+        val flow = BlindBet1OneStepFlow.Initiator(
+            players = listOf(player0, player1, player2, player3),
+            minter = minter, dealer = dealer, smallBet = 4
+        )
+        val future = player1Node.startFlow(flow)
+        network.runNetwork()
+
+        val signedTx = future.getOrThrow()
+        val sum = signedTx.tx.inputs.map {
+            minterNode.services.toStateAndRef<ContractState>(it).state.data
+        }.filter {
+            it is TokenState
+        }.map {
+            it as TokenState
+        }.map {
+            assertEquals(minter, it.minter)
+            assertEquals(player1, it.owner)
+            it.amount
+        }.sum()
+        assertEquals(4, sum)
+    }
+
+    @Test
+    fun `SignedTransaction has 1 output of pot TokenState`() {
+        val flow = BlindBet1OneStepFlow.Initiator(
+            players = listOf(player0, player1, player2, player3),
+            minter = minter, dealer = dealer, smallBet = 4
+        )
+        val future = player1Node.startFlow(flow)
+        network.runNetwork()
+
+        val signedTx = future.getOrThrow()
+        val pots = signedTx.tx.outputsOfType<TokenState>().map {
+            assertEquals(minter, it.minter)
+            assertEquals(player1, it.owner)
+            it
+        }
+        assertEquals(1, pots.size)
+        assertEquals(4, pots[0].amount)
+    }
+
+    @Test
+    fun `SignedTransaction is received by all players and dealer`() {
+        val flow = BlindBet1OneStepFlow.Initiator(
+            players = listOf(player0, player1, player2, player3),
+            minter = minter, dealer = dealer, smallBet = 4
+        )
+        val future = player1Node.startFlow(flow)
+        network.runNetwork()
+
+        val signedTx = future.getOrThrow()
+        for (node in listOf(dealerNode, player0Node, player2Node, player3Node, player1Node)) {
+            assertEquals(signedTx, node.services.validatedTransactions.getTransaction(signedTx.id))
+        }
+    }
+
+    @Test
+    fun `SignedTransaction has expected output of RoundState`() {
+        val flow = BlindBet1OneStepFlow.Initiator(
+            players = listOf(player0, player1, player2, player3),
+            minter = minter, dealer = dealer, smallBet = 4
+        )
+        val future = player1Node.startFlow(flow)
+        network.runNetwork()
+
+        val signedTx = future.getOrThrow()
+        val outputs = signedTx.tx.outputsOfType<RoundState>()
+        assertEquals(1, outputs.size)
+        val deck = dealerNode.transaction {
+            dealerNode.services.cordaService(CardDeckDatabaseService::class.java).getTopDeckRootHashes(1)
+        }
+        assertEquals(
+            RoundState(
+                minter = minter,
+                dealer = dealer,
+                deckRootHash = deck.single(),
+                roundType = BettingRound.BLIND_BET_1,
+                currentPlayerIndex = 1,
+                players = listOf(
+                    PlayedAction(player0, PlayerAction.Missing),
+                    PlayedAction(player1, PlayerAction.Raise),
+                    PlayedAction(player2, PlayerAction.Missing),
+                    PlayedAction(player3, PlayerAction.Missing)
+                ),
+                lastRaiseIndex = 1
+            ),
+            outputs.single()
+        )
+    }
+}
